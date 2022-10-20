fe854ce8fb2a852b6b68e47b703888505791a6da
Emma Nolan <emmanolan2019@gmail.com>
2022-10-20 09:53:26 +0100

    Show movie review excerpts; Refactor Details page.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/favouriteMoviesPage";
 import Movies from "./pages/movieDetailsPage";
 import {Link} from 'react-router-dom'
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/movies/:id" element={<Movies />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
       </Routes>
     </BrowserRouter>
   );
